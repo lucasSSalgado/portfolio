0e7a21e08c14ddd116c30a9c5e53b850fc834414
lucasSSalgado <71613641+lucasSSalgado@users.noreply.github.com>
2025-07-04 12:34:18 -0300

    adding shadcn

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,7 @@
 import {getTranslations, setRequestLocale} from 'next-intl/server';
 
 import {routing} from '@/i18n/routing';
+import { Button } from '@/components/ui/button';
  
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({locale}));
@@ -13,5 +14,8 @@ export default async function HomePage({ params } : { params: Promise<{locale: s
   setRequestLocale(locale);
  
   const t = await getTranslations('HomePage');
-  return <h1 className='bg-red-400'>{t('title')}</h1>;
+  return <div>
+    <h1 className='bg-red-400'>{t('title')}</h1>;
+    <Button className='bg-red-400'>{t('about')}</Button>
+  </div> 
 }
